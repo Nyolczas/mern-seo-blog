8b16d0f7ff2659c9588501186ad13021fe184e7b
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2021-03-04 21:08:34 +0100

    separating routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+// bring routes
+const blogRoutes = require('./routes/blog');
 
 // application
 const app = express();
@@ -27,10 +29,8 @@ if (process.env.NODE_ENV === 'development') {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
-// routes
-app.get('/api', (req, res) => {
-  res.json({ timeLocale: Date().toLocaleString(), time: Date().toString() });
-});
+// routes middleware
+app.use('/api', blogRoutes);
 
 // port
 const port = process.env.PORT || 8000;
